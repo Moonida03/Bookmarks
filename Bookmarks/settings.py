234045e736c2e7ae292f7761c35143afe40a3bf3
Aida <mehrniaaida@gmail.com>
2024-05-28 19:09:13 +0330

    2.4 Done- add dashboard

--- Bookmarks/settings.py
+++ Bookmarks/settings.py
@@ -132,3 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/account/login/'
+LOGIN_URL = '/account/login/'
+LOGOUT_URL = '/account/logout/'
